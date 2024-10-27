67f650f33fb36b566841e0534e0bc49175e4d244
Felippe Pacomio <105086121+FelippePacomio@users.noreply.github.com>
2024-10-27 18:57:16 -0300

    Update lesson2.pl

--- lesson2.pl
+++ lesson2.pl
@@ -5,6 +5,14 @@ gerou(maria, julia). % Maria gerou Julia
 gerou(maria, pedro). % Maria gerou Pedro
 gerou(pedro, lucas). % Pedro gerou Lucas
 
+feminino(maria).
+feminino(bruna).
+feminino(julia).
+feminino(kelly).
+masculino(cleber).
+masculino(pedro).
+masculino(lucas).
+
 % Todos os nomes são escritos em minúsculo. As letras maiúsculas são usadas
 % como variáveis. Por exemplo, se quisermos saber quais são as filhas do
 % Cleber, podemos usar gerou(cleber, X). Sendo o 'X' a variável.
@@ -13,3 +21,5 @@ gerou(pedro, lucas). % Pedro gerou Lucas
 % Exemplo para saber quem são os avós: gerou(X, Y), gerou(Y, pedro).
 % Exemplo para saber quem são os netos: gerou(cleber, Y), gerou(Y, X).
 % Segundo exemplo para saber quem são os avós: gerou(X, Y), gerou(Y, Z).
+
+% Exemplo para saber quem é a avó: gerou(X, Y), gerou(Y, pedro), feminino(X).
